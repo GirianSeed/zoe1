5a7cc8a3fce8755f1e895c8f36dff7a5e7aff33e
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-18 14:13:14 -0500

    Match FS_UploadVu0Program

--- source/libfs/vu0man.cc
+++ source/libfs/vu0man.cc
@@ -1,13 +1,15 @@
 #include <stddef.h>
 #include <libdma.h>
 
+// CXXFLAGS = -G0 -O2 -fno-exceptions
+
 static void *pcurProgDma = NULL;
 
 void FS_UploadVu0Program( void *pDmaTag )
 {
     sceDmaChan *pDmaVif0;
 
-    if (pDmaTag != pcurProgDma)
+    if (pcurProgDma != pDmaTag)
     {
         pDmaVif0 = sceDmaGetChan( SCE_DMA_VIF0 );
         pDmaVif0->chcr.TTE = 1;
